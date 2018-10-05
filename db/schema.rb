32a4aaa7c0487b1184f2f7ad14d0950fa73647c8
dwk5 <dwk5@yandex.ru>
2018-10-05 14:42:22 +0300

    remove and add references in migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_142238) do
+ActiveRecord::Schema.define(version: 2018_10_05_114022) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false
