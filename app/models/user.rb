43502d7cb5cae42c320d5d8c0417e01fc7fb764b
dwk5 <dwk5@yandex.ru>
2018-10-12 07:38:16 +0300

    add assosiations and ER diagram

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :passed_tests, dependent: :destroy
   has_many :tests, through: :passed_tests
+  has_many :author_tests, class_name: "Test", foreign_key: "author_id"
 
   def test_by_level(level)
     tests.where(level: level)
