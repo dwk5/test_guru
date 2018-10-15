049d771d088e73599b5bbe43d970a394fa26c28e
dwk5 <dwk5@yandex.ru>
2018-10-15 18:49:10 +0300

    fix scopes and methods

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,8 @@
 class Answer < ApplicationRecord
   belongs_to :question
-  scope :correct_answers, -> { where(correct: true)}
+
+  scope :correct, -> { where(correct: true)}
+
   validates :body, presence: true
   validate :amount_answers, on: :create
 
