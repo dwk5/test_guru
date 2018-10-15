c7f85e18a9b20d08c4c60488e88dc461abd5a005
dwk5 <dwk5@yandex.ru>
2018-10-15 20:37:15 +0300

    fix migration

--- db/migrate/20181015150617_add_index_test_level.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexTestLevel < ActiveRecord::Migration[5.2]
-  def change
-    add_index(:tests, [:title, :level], unique: true)
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_150617) do
+ActiveRecord::Schema.define(version: 2018_10_15_173030) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false
