2d098d01fe80550241b6a9db366a65faeaee94c3
dwk5 <dwk5@yandex.ru>
2018-11-29 22:18:32 +0300

    part16 Data Attributes

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,10 +1,10 @@
 class Admin::TestsController < Admin::BaseController
 
-  before_action :set_test, only: %i[edit update show destroy]
+  before_action :set_tests, only: %i[index update_inline]
+  before_action :set_test, only: %i[edit update show destroy update_inline]
 
   def index
-    flash[:notice] = "Привет, #{current_user.first_name}"
-    @tests = Test.all
+    flash[:notice] = "Hello, #{current_user.first_name}"
   end
 
   def show
@@ -34,12 +34,24 @@ class Admin::TestsController < Admin::BaseController
     end
   end
 
+  def update_inline
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render :index
+    end
+  end
+
   def destroy
     @test.destroy
     redirect_to admin_tests_path
   end
 
   private
+
+  def set_tests
+    @tests = Test.all
+  end
 
   def test_params
     params.require(:test).permit(:title, :level, :category_id)
