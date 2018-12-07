de8e5ac5873c3ebe85a0767c992cc84b8f125d1a
dwk5 <dwk5@yandex.ru>
2018-12-07 11:30:09 +0300

    fix answers correct boolean migration

--- db/migrate/20181004132412_change_answer_correct.rb
+++
@@ -1,9 +0,0 @@
-class ChangeAnswerCorrect < ActiveRecord::Migration[5.2]
-  def up
-    change_column :answers, :correct, :boolean, default: false
-  end
-
-  def down
-    change_column :answers, :correct, :boolean, default: true
-  end
-end
