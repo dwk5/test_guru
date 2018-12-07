bb742bd2003b7ce6d2ea483c0cef8e3fd8b72fe1
dwk5 <dwk5@yandex.ru>
2018-12-07 10:38:40 +0300

    fix answers correct boolean

---
+++ db/migrate/20181207072318_answer_correct_boolean.rb
@@ -0,0 +1,5 @@
+class AnswerCorrectBoolean < ActiveRecord::Migration[5.2]
+  def change
+    change_column :answers, :correct, :boolean, default: :false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_122253) do
+ActiveRecord::Schema.define(version: 2018_12_07_072318) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
-    t.boolean "correct", default: false
+    t.boolean "correct", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "question_id"
